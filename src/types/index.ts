bdbf0fe78369558dfbcb9686f9d6ec141205d182
julianabot <julianarolluqui@gmail.com>
2025-07-24 01:40:25 +0800

    feat: add field types

---
+++ src/types/index.ts
@@ -0,0 +1,15 @@
+type Field =
+  | "Input"
+  | "Textarea"
+  | "File Input"
+  | "Checkbox"
+  | "Combobox"
+  | "Radio Group"
+  | "Date Picker"
+  | "Time Picker"
+  | "Datetime Picker"
+  | "Select"
+  | "Switch";
+
+//TODO: File Input, Combobox, Date Picker, Time Picker, Datetime Picker components are not implemented yet
+export type { Field };
